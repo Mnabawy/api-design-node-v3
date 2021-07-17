c62f1c9f4dcff9497574625bd09447d6dfe69ade
Mnabawy <m_ninja61@yahoo.com>
2021-07-17 16:59:45 +0200

    crud routes added to item resource

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,21 @@
 import { Router } from 'express'
+import controllers from './item.controllers'
 
 const router = Router()
+
+const controller = (req, res) => {
+  res.send('controller works fine')
+}
+
+router
+  .route('/')
+  .get(controller)
+  .post(controller)
+
+router
+  .route('/:id')
+  .get(controller)
+  .put(controller)
+  .delete(controller)
 
 export default router
