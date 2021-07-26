c5852fcefe6f05301eb23be0b3b6f557de200c23
Mnabawy <m_ninja61@yahoo.com>
2021-07-26 09:54:33 +0200

    protect routes

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -60,5 +60,29 @@ export const signin = async (req, res) => {
 }
 
 export const protect = async (req, res, next) => {
+  const bearer = req.headers.authorization
+
+  if (!bearer || !bearer.startsWith('Bearer ')) {
+    return res.status(401).end()
+  }
+
+  const token = bearer.split('Bearer ')[1].trim()
+  let payload
+  try {
+    payload = await verifyToken(token)
+  } catch (e) {
+    return res.status(401).end()
+  }
+
+  const user = await User.findById(payload.id)
+    .select('-password')
+    .lean()
+    .exec()
+
+  if (!user) {
+    return res.status(401).end()
+  }
+
+  req.user = user
   next()
 }
